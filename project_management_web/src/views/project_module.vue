39a4a4cf882f471a46418bb322cdec6f088f0835
haidee <2512265671@qq.com>
2018-07-09 16:38:20 +0800

    Revert: Revert: user develop

--- project_management_web/src/views/project_module.vue
+++ project_management_web/src/views/project_module.vue
@@ -1,6 +1,42 @@
 <template>
-   <div>
-     项目模块正在开发中....
+   <div style="width: 900px;padding:  10px 20px">
+     <el-row>
+       <el-button style="float:right" type="primary" @click="moduleFormOpen('add')"  round>新增模块</el-button>
+     </el-row>
+     <el-table :data="projectList" style="width: 100%;margin-top:10px;">
+       <el-table-column label="序号" type="index"></el-table-column>
+       <el-table-column prop="module_name" label="模块名称"></el-table-column>
+       <el-table-column prop="module_developer" label="开发人员"></el-table-column>
+       <el-table-column label="操作">
+         <template slot-scope="scope">
+           <el-button type="primary" title="编辑" size="small" @click="moduleFormOpen('edit',scope.row)" icon="el-icon-edit" circle></el-button>
+           <el-button type="danger" size="small" icon="el-icon-delete" @click="moduleDeleteOpen(scope.row.module_id)" circle></el-button>
+         </template>
+       </el-table-column>
+     </el-table>
+     <el-dialog title="添加项目模块" :visible.sync="dialogTableVisible">
+       <el-form :model="modular">
+         <el-form-item label="模块名称">
+           <el-input  placeholder="请输入模块名称" v-model="modular.module_name"></el-input>
+         </el-form-item>
+         <el-form-item label="开发人员">
+           <el-input  placeholder="请输入开发人员" v-model="modular.module_developer"></el-input>
+         </el-form-item>
+       </el-form>
+       <div slot="footer" class="dialog-footer">
+         <el-button @click="dialogTableVisible = false">取消</el-button>
+         <el-button type="primary" @click="projectSubmit">确定</el-button>
+       </div>
+     </el-dialog>
+     <el-dialog title="删除项目模块" :visible.sync="dialogDeleteVisible">
+       <div class="fs16 danger">
+         确定要删除该项目模块吗？
+       </div>
+       <div slot="footer" class="dialog-footer">
+         <el-button @click="dialogDeleteVisible = false">取消</el-button>
+         <el-button type="primary" @click="deleteSubmit">确定</el-button>
+       </div>
+     </el-dialog>
    </div>
 
 
@@ -10,12 +46,12 @@
     data() {
       return {
         projectList: [],
-        projectFormVisible: false,
-        projectStatusOptions:[
-          '进行中','已完成'
-        ],
-        projectForm: {
-          project_name: ''
+        dialogDeleteVisible: false,
+        dialogTableVisible: false,
+        delId:'',
+        modular: {
+          module_name: '',
+          module_developer:''
         },
         projectRules: {
           project_name: [
@@ -33,7 +69,7 @@
         var vm=this;
         vm.$http({
           method: 'get',
-          url: vm.config.baseUrl+'project/getProjectList'
+          url: vm.config.baseUrl+'project/projectModuelList'
         }).then(function(response) {
           var data=response.data
           vm.projectList=data.projectList
@@ -42,47 +78,69 @@
           console.log(response)
         })
       },
-      projectFormOpen:function () {
-        this.projectFormVisible = true;
-        this.projectOpenNum++;
-        if(this.projectOpenNum>1){
-          this.$refs['projectForm'].resetFields();
+      moduleFormOpen:function (type,obj) {
+        this.dialogTableVisible = true;
+        this.objType = type;
+        if(type=='edit'){
+          this.modular = obj;
+        }else{
+          this.modular = {
+            module_name:'',
+            module_developer:''
+          }
         }
-        this.projectForm={
-            project_name:null,
-            company_name:null,
-            project_period:null,
-            project_status:null,
-            project_desc:null
-        };
 
+      },
+      moduleDeleteOpen:function(deleteId){
+        this.dialogDeleteVisible = true;
+        this.delId = deleteId;
       },
       projectSubmit:function () {
         var vm=this;
-        vm.$refs['projectForm'].validate((valid) => {
-          if (valid) {
-            var projectInfo = this.projectForm;
-            if(projectInfo.project_period!=null&&projectInfo.project_period.length>1){
-              projectInfo.project_begin=vm.$moment(projectInfo.project_period[0]).format("YYYY-MM-DD");
-              projectInfo.project_end=vm.$moment(projectInfo.project_period[1]).format("YYYY-MM-DD");
-
-            }
+        /*vm.$refs['projectForm'].validate((valid) => {
+          if (valid) {*/
+            var projectInfo = this.modular;
+            projectInfo.project_id = this.$route.params.projectId;
             console.log(projectInfo)
+            var url = '';
+            if(this.objType=='edit'){
+              url = 'project/updateProjectModuel';
+            }else{
+              url = 'project/addProjectModuel';
+            }
             vm.$http({
               method: 'POST',
-              url: this.config.baseUrl + 'project/addProject',
+              url: this.config.baseUrl + url,
               data: projectInfo
             }).then(function (data) {
               var result = data.data;
               var response = result.code;
               if (response == 0) {
-                vm.projectFormVisible = false;
+                vm.dialogTableVisible = false;
                 vm.$message({message: '提交成功！！', type: 'success'});
                 vm.getProjectList()
               } else {
                 vm.$message.error('提交失败！！');
               }
             })
+          /*}
+        })*/
+      },
+      deleteSubmit:function(){
+        var vm=this;
+        vm.$http({
+          method: 'POST',
+          url: this.config.baseUrl + 'project/deleteProjectModuel',
+          data: {module_id:this.delId}
+        }).then(function (data) {
+          var result = data.data;
+          var response = result.code;
+          if (response == 0) {
+            vm.dialogDeleteVisible = false;
+            vm.$message({message: '删除成功！！', type: 'success'});
+            vm.getProjectList()
+          } else {
+            vm.$message.error('删除失败！！');
           }
         })
       }
