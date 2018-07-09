bb81b54a78e2d05133e0cc75b824fcf4457c4f41
Jerry-web <13811268228@163.com>
2018-07-09 17:18:18 +0800

    Revert: project module

--- project_management_web/build/webpack.base.conf.js
+++ project_management_web/build/webpack.base.conf.js
@@ -66,7 +66,6 @@ module.exports = {
         }
       }
     ]
-
   },
   node: {
     // prevent webpack from injecting useless setImmediate polyfill because Vue
--- project_management_web/src/views/project_home.vue
+++ project_management_web/src/views/project_home.vue
@@ -1,164 +1,157 @@
 <template>
-  <div>
-    <div style="height:50px;background: #eee;margin: -20px;line-height: 50px" class="margin-bottom-20">
-      <el-breadcrumb separator-class="el-icon-arrow-right" style="display: inline-block">
-        <el-breadcrumb-item class="margin-left-20">项目首页</el-breadcrumb-item>
-      </el-breadcrumb>
-    </div>
-    <div style="width: 1000px;padding:  10px 20px">
-      <div class="itme-title">项目基本信息
-        <el-button type="primary"  style="float: right;margin-top: -5px"  size="small" plain @click="projectFormOpen"><i class="fa fa-edit margin-right-5"></i>修改项目</el-button>
-      </div>
-      <div class="padding-10">
-        <table class="table table-bordered  fs14 home-table" >
-          <tbody>
-          <tr>
-            <td width="150" class="info-title">项目名称</td>
-            <td colspan="3">{{projectInfo.project_name}}</td>
-          </tr>
-          <tr>
-            <td width="150" class="info-title">公司名称</td>
-            <td colspan="3">{{projectInfo.project_company}}</td>
-          </tr>
-          <tr>
-            <td width="150" class="info-title">项目周期</td>
-            <td colspan="3">{{projectInfo.project_begin}} 至 {{projectInfo.project_end}}</td>
-          </tr>
-          <tr>
-            <td width="150" class="info-title">项目版本</td>
-            <td width="320">{{projectInfo.project_version}}</td>
-            <td width="150" class="info-title">项目状态</td>
-            <td><span v-bind:class="{warning:projectInfo.project_status=='进行中',success:projectInfo.project_status=='已完成'}">{{projectInfo.project_status}}</span></td>
+   <div style="width: 900px;padding:  10px 20px">
+     <div class="itme-title">项目基本信息
+       <el-button type="primary"  style="float: right;margin-top: -5px" class=" margin-right-10" size="small" plain @click="projectFormOpen"><i class="fa fa-edit margin-right-5"></i>修改项目</el-button>
+     </div>
+     <div class="padding-10">
+       <table class="table table-bordered  fs14 home-table" >
+         <tbody>
+         <tr>
+           <td width="150" class="info-title">项目名称</td>
+           <td>{{projectInfo.project_name}}</td>
+         </tr>
+         <tr>
+           <td width="150" class="info-title">公司名称</td>
+           <td>{{projectInfo.project_company}}</td>
+         </tr>
+         <tr>
+           <td width="150" class="info-title">项目周期</td>
+           <td>{{projectInfo.project_begin}} 至 {{projectInfo.project_end}}</td>
+         </tr>
+         <tr>
+           <td width="150" class="info-title">项目版本</td>
+           <td>{{projectInfo.project_version}}</td>
+         </tr>
+         <tr>
+           <td width="150" class="info-title">开发人员</td>
+           <td>{{projectInfo.project_developer}}</td>
+         </tr>
+         <tr>
+           <td width="150" class="info-title">项目状态</td>
+           <td><span v-bind:class="{warning:projectInfo.project_status=='进行中',success:projectInfo.project_status=='已完成'}">{{projectInfo.project_status}}</span></td>
+         </tr>
+         <tr>
+           <td width="150" class="info-title">项目描述</td>
+           <td>{{projectInfo.project_desc}}</td>
+         </tr>
+         </tbody>
+       </table>
+     </div>
+     <div class="itme-title margin-top-20">项目网址列表
+       <el-button type="success"  style="float: right;margin-top: -5px" class=" margin-right-10" size="small" plain @click="urlFormOpen('add')"><i class="fa fa-plus margin-right-5"></i>新增网址</el-button>
 
-          </tr>
-          <tr>
-            <td width="150" class="info-title">开发人员</td>
-            <td colspan="3">{{projectInfo.project_developer}}</td>
-          </tr>
-          <tr>
-            <td width="150" class="info-title">项目描述</td>
-            <td colspan="3">{{projectInfo.project_desc}}</td>
-          </tr>
-          </tbody>
-        </table>
-      </div>
-      <div class="itme-title margin-top-20">项目网址列表
-        <el-button type="success"  style="float: right;margin-top: -5px"  size="small" plain @click="urlFormOpen('add')"><i class="fa fa-plus margin-right-5"></i>新增网址</el-button>
-
-      </div>
-      <div class="padding-10">
-        <el-table
-          :data="projectInfo.ProjectUrls"
-          border
-          style="width: 100%">
-          <el-table-column
-            type="index"
-          >
-          </el-table-column>
-          <el-table-column
+     </div>
+     <div class="padding-10">
+       <el-table
+         :data="projectInfo.ProjectUrls"
+         border
+         style="width: 100%">
+         <el-table-column
+           type="index"
+         >
+         </el-table-column>
+         <el-table-column
             width="150"
-            label="网址名称"
-            prop="project_url_name" >
+           label="网址名称"
+           prop="project_url_name" >
 
-          </el-table-column>
-          <el-table-column
-            header-align="center"
-            label="URL"
-            prop="project_url">
-            <template slot-scope="scope" >
-              <a class="info" target="_blank" v-bind:href="scope.row.project_url">{{scope.row.project_url}}</a>
-            </template>
-          </el-table-column>
-          <el-table-column
-            width="100"
-            header-align="center"
-            class-name="text-center"
-            label="操作"
-            prop="project_status">
-            <template slot-scope="scope" >
-              <el-button type="primary" title="编辑" size="small" @click="urlFormOpen('edit',scope.row)" icon="el-icon-edit" circle></el-button>
-              <el-button type="danger" size="small" icon="el-icon-delete" @click="urlDeleteOpen(scope.row.project_url_id)" circle></el-button>
-            </template>
-          </el-table-column>
+         </el-table-column>
+         <el-table-column
+           header-align="center"
+           label="URL"
+           prop="project_url">
+           <template slot-scope="scope" >
+             <a class="info" target="_blank" v-bind:href="scope.row.project_url">{{scope.row.project_url}}</a>
+           </template>
+         </el-table-column>
+         <el-table-column
+           width="100"
+           header-align="center"
+           class-name="text-center"
+           label="操作"
+           prop="project_status">
+           <template slot-scope="scope" >
+             <el-button type="primary" title="编辑" size="small" @click="urlFormOpen('edit',scope.row)" icon="el-icon-edit" circle></el-button>
+             <el-button type="danger" size="small" icon="el-icon-delete" @click="urlDeleteOpen(scope.row.project_url_id)" circle></el-button>
+           </template>
+         </el-table-column>
 
-        </el-table>
-      </div>
-      <el-dialog class="add-project" width="700px" title="修改项目" :visible.sync="projectFormVisible" :close-on-click-modal="false"  >
-        <el-form :model="projectForm" :rules="projectRules" label-width="80px"  ref="projectForm"  style="padding: 0 20px">
-          <el-form-item label="项目名称" prop="project_name" >
-            <el-input  placeholder="请输入项目名称" v-model="projectForm.project_name">
-            </el-input>
-          </el-form-item>
-          <el-form-item label="公司名称" prop="project_company" >
-            <el-input  placeholder="请输入公司名称" v-model="projectForm.project_company">
-            </el-input>
-          </el-form-item>
-          <el-form-item label="项目周期" prop="project_period" >
-            <el-date-picker style="width: 100%"
-                            v-model="projectForm.project_period"
-                            type="daterange"
-                            align="right"
-                            unlink-panels
-                            range-separator="至"
-                            start-placeholder="开始日期"
-                            end-placeholder="结束日期">
-            </el-date-picker>
-          </el-form-item>
-          <el-form-item label="项目版本" prop="project_version" >
-            <el-input  placeholder="请输入项目版本" v-model="projectForm.project_version">
-            </el-input>
-          </el-form-item>
-          <el-form-item label="开发人员" prop="project_developer" >
-            <el-input  placeholder="请输入开发人员" v-model="projectForm.project_developer">
-            </el-input>
-          </el-form-item>
-          <el-form-item label="项目状态" prop="project_status" >
-            <el-select v-model="projectForm.project_status" style="width: 100%" placeholder="请选择项目状态">
-              <el-option
-                v-for="item in projectStatusOptions"
-                :key="item"
-                :label="item"
-                :value="item">
-              </el-option>
-            </el-select>
-          </el-form-item>
-          <el-form-item label="项目介绍" prop="project_desc">
-            <el-input type="textarea" rows="3" placeholder="请输入项目介绍" v-model="projectForm.project_desc"></el-input>
-          </el-form-item>
-        </el-form>
-        <div slot="footer" class="dialog-footer">
-          <el-button @click.native="projectFormVisible = false">取消</el-button>
-          <el-button type="primary" @click.native="projectSubmit" >提交</el-button>
-        </div>
-      </el-dialog>
-      <el-dialog class="url-panel" width="700px" title="修改网址" :visible.sync="urlFormVisible" :close-on-click-modal="false"  >
-        <el-form :model="urlForm" :rules="urlRules" label-width="80px"  ref="urlForm"  style="padding: 0 20px">
-          <el-form-item label="网址名称" prop="project_url_name" >
-            <el-input  placeholder="请输入网址名称" v-model="urlForm.project_url_name">
-            </el-input>
-          </el-form-item>
-          <el-form-item label="URL" prop="project_url" >
-            <el-input  placeholder="请输入URL" v-model="urlForm.project_url">
-            </el-input>
-          </el-form-item>
-        </el-form>
-        <div slot="footer" class="dialog-footer">
-          <el-button @click.native="urlFormVisible = false">取消</el-button>
-          <el-button type="primary" @click.native="urlSubmit" >提交</el-button>
-        </div>
-      </el-dialog>
-      <el-dialog class="delete-panel" width="400px" title="删除网址" :visible.sync="urlDeleteVisible" :close-on-click-modal="false"  >
-        <div class="fs16 danger">
-          确定要删除该网址吗？
-        </div>
-        <div slot="footer" class="dialog-footer">
-          <el-button @click.native="urlDeleteVisible = false">取消</el-button>
-          <el-button type="primary" @click.native="urlDelete" >提交</el-button>
-        </div>
-      </el-dialog>
-    </div>
-  </div>
-
+       </el-table>
+     </div>
+     <el-dialog class="add-project" width="700px" title="修改项目" :visible.sync="projectFormVisible" :close-on-click-modal="false"  >
+       <el-form :model="projectForm" :rules="projectRules" label-width="80px"  ref="projectForm"  style="padding: 0 20px">
+         <el-form-item label="项目名称" prop="project_name" >
+           <el-input  placeholder="请输入项目名称" v-model="projectForm.project_name">
+           </el-input>
+         </el-form-item>
+         <el-form-item label="公司名称" prop="project_company" >
+           <el-input  placeholder="请输入公司名称" v-model="projectForm.project_company">
+           </el-input>
+         </el-form-item>
+         <el-form-item label="项目周期" prop="project_period" >
+           <el-date-picker style="width: 100%"
+                           v-model="projectForm.project_period"
+                           type="daterange"
+                           align="right"
+                           unlink-panels
+                           range-separator="至"
+                           start-placeholder="开始日期"
+                           end-placeholder="结束日期">
+           </el-date-picker>
+         </el-form-item>
+         <el-form-item label="项目版本" prop="project_version" >
+           <el-input  placeholder="请输入项目版本" v-model="projectForm.project_version">
+           </el-input>
+         </el-form-item>
+         <el-form-item label="开发人员" prop="project_developer" >
+           <el-input  placeholder="请输入开发人员" v-model="projectForm.project_developer">
+           </el-input>
+         </el-form-item>
+         <el-form-item label="项目状态" prop="project_status" >
+           <el-select v-model="projectForm.project_status" style="width: 100%" placeholder="请选择项目状态">
+             <el-option
+               v-for="item in projectStatusOptions"
+               :key="item"
+               :label="item"
+               :value="item">
+             </el-option>
+           </el-select>
+         </el-form-item>
+         <el-form-item label="项目介绍" prop="project_desc">
+           <el-input type="textarea" rows="3" placeholder="请输入项目介绍" v-model="projectForm.project_desc"></el-input>
+         </el-form-item>
+       </el-form>
+       <div slot="footer" class="dialog-footer">
+         <el-button @click.native="projectFormVisible = false">取消</el-button>
+         <el-button type="primary" @click.native="projectSubmit" >提交</el-button>
+       </div>
+     </el-dialog>
+     <el-dialog class="url-panel" width="700px" title="修改网址" :visible.sync="urlFormVisible" :close-on-click-modal="false"  >
+       <el-form :model="urlForm" :rules="urlRules" label-width="80px"  ref="urlForm"  style="padding: 0 20px">
+         <el-form-item label="网址名称" prop="project_url_name" >
+           <el-input  placeholder="请输入网址名称" v-model="urlForm.project_url_name">
+           </el-input>
+         </el-form-item>
+         <el-form-item label="URL" prop="project_url" >
+           <el-input  placeholder="请输入URL" v-model="urlForm.project_url">
+           </el-input>
+         </el-form-item>
+       </el-form>
+       <div slot="footer" class="dialog-footer">
+         <el-button @click.native="urlFormVisible = false">取消</el-button>
+         <el-button type="primary" @click.native="urlSubmit" >提交</el-button>
+       </div>
+     </el-dialog>
+     <el-dialog class="delete-panel" width="400px" title="删除网址" :visible.sync="urlDeleteVisible" :close-on-click-modal="false"  >
+       <div class="fs16 danger">
+         确定要删除该网址吗？
+       </div>
+       <div slot="footer" class="dialog-footer">
+         <el-button @click.native="urlDeleteVisible = false">取消</el-button>
+         <el-button type="primary" @click.native="urlDelete" >提交</el-button>
+       </div>
+     </el-dialog>
+   </div>
 
 
 </template>
