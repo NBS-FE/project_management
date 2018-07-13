9493c4b32eaa6a193fcd40f8e7b6944523d0b53c
haidee <2512265671@qq.com>
2018-07-13 15:41:33 +0800

    Revert: Revert: user develop

--- project_management_web/src/components/TreeTable/index.vue
+++ project_management_web/src/components/TreeTable/index.vue
@@ -17,8 +17,7 @@
           <i v-if="!scope.row._expanded" class="el-icon-caret-right"></i>
           <i v-else class="el-icon-caret-bottom"></i>
         </span>
-        <span v-html="scope.row[column.value]"></span>
-        <!--{{scope.row[column.value]}}-->
+        {{scope.row[column.value]}}
       </template>
     </el-table-column>
     <slot></slot>
