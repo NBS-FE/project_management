b5c76cfc25defcbce4622b620ead2884f4d47a5b
haidee <2512265671@qq.com>
2018-07-24 16:43:27 +0800

    Revert: Revert: user develop

--- project_management_web/src/router/index.js
+++ project_management_web/src/router/index.js
@@ -19,8 +19,16 @@ export default new Router({
         { path: 'bugdetail/:projectId/:bugId', component: resolve => require(['../views/project_bug_detail.vue'], resolve) },
       ]
     },
-    { path: '/userList', component: resolve => require(['../views/user_list.vue'], resolve)},
-    { path: '/register', component: resolve => require(['../views/register.vue'], resolve)}
+    { path: '/common', component: resolve => require(['../views/common.vue'], resolve),
+      children:[
+        { path: 'userList', component: resolve => require(['../views/user_list.vue'], resolve) },
+        { path: 'weekReport', component: resolve => require(['../views/week_report.vue'], resolve) },
+        { path: 'weekDetail/:week_report_id', component: resolve => require(['../views/week_detail.vue'], resolve) }
+      ]
+    },
+    // { path: '/userList', component: resolve => require(['../views/user_list.vue'], resolve)},
+    { path: '/register', component: resolve => require(['../views/register.vue'], resolve)},
+    { path: '/login', component: resolve => require(['../views/login.vue'], resolve)}
 
   ]
 })
